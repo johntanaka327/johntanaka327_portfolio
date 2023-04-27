7f1f008db862ddb2f7dc47672ab51b78f82b8ad8
tomvictor303 <tomvictor303@gmail.com>
2023-04-26 22:07:13 -0400

    Updates

---
+++ precache-manifest.b8c7cd293e8eb0684a521067f302d2ee.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ad5d8fc5c9e2a86541e791fbd8f9a79",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f4612171f8da5e2951e",
+    "url": "/static/css/2.14351861.chunk.css"
+  },
+  {
+    "revision": "9fdb966d1166999791ee",
+    "url": "/static/css/main.a640f64d.chunk.css"
+  },
+  {
+    "revision": "6f4612171f8da5e2951e",
+    "url": "/static/js/2.ea4bc080.chunk.js"
+  },
+  {
+    "revision": "be6829ae46fa1d5f64ff1f06147ffe7e",
+    "url": "/static/js/2.ea4bc080.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9fdb966d1166999791ee",
+    "url": "/static/js/main.02e40a52.chunk.js"
+  },
+  {
+    "revision": "79d439c7be0dd499b205",
+    "url": "/static/js/runtime-main.e9933245.js"
+  },
+  {
+    "revision": "ce9b13e58fdd40e48635dd1de6aec90c",
+    "url": "/static/media/Hello.ce9b13e5.jpg"
+  },
+  {
+    "revision": "38828042bce3def5dbc3c5b4a5d2df93",
+    "url": "/static/media/Pencil.38828042.jpg"
+  },
+  {
+    "revision": "ef1ed0108a07992e874d11066e1a06c7",
+    "url": "/static/media/bookheap.ef1ed010.png"
+  },
+  {
+    "revision": "f3b3efd4f444f450f6692bb96e2cd1b2",
+    "url": "/static/media/chess.f3b3efd4.png"
+  },
+  {
+    "revision": "f93a88f82aed103026e8c4fc7cfb7adb",
+    "url": "/static/media/definition.f93a88f8.png"
+  },
+  {
+    "revision": "7a0505a9d3c6947574623bac509b96f8",
+    "url": "/static/media/memes.7a0505a9.png"
+  },
+  {
+    "revision": "dd77e9e7914aa7a71a1718dc53b2be8c",
+    "url": "/static/media/sam.dd77e9e7.jpg"
+  },
+  {
+    "revision": "fc269d8013022a5e02fdd23f0c790b1d",
+    "url": "/static/media/shopping-app.fc269d80.png"
+  },
+  {
+    "revision": "38dcf970971c80745f27e3f4c12ecb4b",
+    "url": "/static/media/superhero.38dcf970.png"
+  },
+  {
+    "revision": "794aaa6ee350764f56fd61ab01c08b2e",
+    "url": "/static/media/travel-log.794aaa6e.png"
+  }
+]);
